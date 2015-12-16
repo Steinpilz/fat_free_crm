07d1b11faaabd261b7ff0abab569548e36d49b4d
ivanbenko <bivans209@gmail.com>
2015-12-16 11:36:14 +0100

    Fix app name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,4 @@
 if FatFreeCRM.application?
   require 'fat_free_crm/secret_token_generator'
-  Hours::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+  FatFreeCRM::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
 end
